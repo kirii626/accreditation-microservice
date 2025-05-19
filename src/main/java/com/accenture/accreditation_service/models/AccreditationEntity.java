03e246b41c4f573d4a37c28df86512d02e9b9e36
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-05-19 10:39:22 -0300

    change Generation.TYPE from AUTO to IDENTITY

--- src/main/java/com/accenture/accreditation_service/models/AccreditationEntity.java
+++ src/main/java/com/accenture/accreditation_service/models/AccreditationEntity.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 public class AccreditationEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long accreditationId;
 
     @Setter
