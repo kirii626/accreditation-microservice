7d9064b9a5535e44445496e50160435d7d68fdf7
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-05-19 11:29:08 -0300

    add AccreditationEventPublisher

---
+++ src/main/java/com/accenture/accreditation_service/events/publishers/AccreditationEventPublisher.java
@@ -0,0 +1,24 @@
+package com.accenture.accreditation_service.events.publishers;
+
+import com.accenture.accreditation_service.dtos.AccreditationDtoOutput;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class AccreditationEventPublisher {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("{accreditation.exchange}")
+    private String exchange;
+
+    @Value("${rabbitmq.routingkey.accreditation}")
+    private String accreditationCreatedRoutingKey;
+
+    public void sendAccreditationCreatedNotification(AccreditationDtoOutput accreditationDtoOutput) {
+        rabbitTemplate.convertAndSend(exchange, accreditationCreatedRoutingKey, accreditationDtoOutput);
+    }
+}
