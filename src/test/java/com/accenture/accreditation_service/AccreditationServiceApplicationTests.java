ced6556e5a33103e351e0b735088eb1023120afa
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-05-26 10:00:14 -0300

    use ActiveProfiles annotation to use the test profile

--- src/test/java/com/accenture/accreditation_service/AccreditationServiceApplicationTests.java
+++ src/test/java/com/accenture/accreditation_service/AccreditationServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.accenture.accreditation_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AccreditationServiceApplicationTests {
 
 	@Test
