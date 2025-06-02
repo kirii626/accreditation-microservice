424810625e9fb35dbd0f88195849a322d88220c7
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-06-02 16:53:52 -0300

    define configuration for send a header with a token on synchronous calls

--- src/main/java/com/accenture/accreditation_service/config/FeignClientConfig.java
+++ src/main/java/com/accenture/accreditation_service/config/FeignClientConfig.java
@@ -1,14 +1,29 @@
 package com.accenture.accreditation_service.config;
 
+import feign.RequestInterceptor;
 import feign.codec.ErrorDecoder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class FeignClientConfig {
+
+    @Value("${internal.secret.token}")
+    private String internalToken;
 
     @Bean
     public ErrorDecoder errorDecoder() {
         return new CustomFeignErrorDecoder();
+    }
+
+    @Bean
+    public RequestInterceptor internalRequestInterceptor() {
+        return requestTemplate -> {
+            requestTemplate.header("X-Internal-Token", internalToken);
+            log.info("Adding internal token to request: {}", internalToken);
+        };
     }
 }
